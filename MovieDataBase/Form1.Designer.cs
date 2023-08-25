d67b718c53104bc9a1b1ccf0bb020d3800db2cbb
Denis <den.nomok@yandex.ru>
2023-08-25 13:33:40 +0900

    First version List of Movie application

---
+++ MovieDataBase/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace MovieDataBase
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle46 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle47 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle48 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.button = new System.Windows.Forms.Button();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+			this.buttonUpdate = new System.Windows.Forms.Button();
+			this.buttonDelete = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.label5 = new System.Windows.Forms.Label();
+			this.tableLayoutPanel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(238, 51);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(232, 22);
+			this.textBox1.TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(238, 102);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(232, 22);
+			this.textBox2.TabIndex = 1;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(238, 150);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(232, 22);
+			this.textBox3.TabIndex = 2;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(238, 203);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(232, 22);
+			this.textBox4.TabIndex = 3;
+			// 
+			// button
+			// 
+			this.button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(58)))), ((int)(((byte)(81)))));
+			this.button.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button.ForeColor = System.Drawing.Color.WhiteSmoke;
+			this.button.Location = new System.Drawing.Point(46, 284);
+			this.button.Name = "button";
+			this.button.Size = new System.Drawing.Size(146, 55);
+			this.button.TabIndex = 4;
+			this.button.Text = "Insert";
+			this.button.UseVisualStyleBackColor = false;
+			this.button.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 1;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 0);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 51);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 1;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(597, 288);
+			this.tableLayoutPanel1.TabIndex = 5;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+			this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+			this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(66)))), ((int)(((byte)(91)))));
+			this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+			this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+			dataGridViewCellStyle46.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+			dataGridViewCellStyle46.BackColor = System.Drawing.SystemColors.HotTrack;
+			dataGridViewCellStyle46.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			dataGridViewCellStyle46.ForeColor = System.Drawing.Color.White;
+			dataGridViewCellStyle46.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+			dataGridViewCellStyle46.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+			dataGridViewCellStyle46.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+			this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle46;
+			this.dataGridView1.ColumnHeadersHeight = 29;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+			this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridView1.EnableHeadersVisualStyles = false;
+			this.dataGridView1.GridColor = System.Drawing.Color.SteelBlue;
+			this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+			this.dataGridView1.MultiSelect = false;
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+			dataGridViewCellStyle47.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle47.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(66)))), ((int)(((byte)(91)))));
+			dataGridViewCellStyle47.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			dataGridViewCellStyle47.ForeColor = System.Drawing.Color.White;
+			dataGridViewCellStyle47.SelectionBackColor = System.Drawing.Color.SteelBlue;
+			dataGridViewCellStyle47.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+			dataGridViewCellStyle47.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+			this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle47;
+			this.dataGridView1.RowHeadersWidth = 35;
+			dataGridViewCellStyle48.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(66)))), ((int)(((byte)(91)))));
+			dataGridViewCellStyle48.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			dataGridViewCellStyle48.ForeColor = System.Drawing.Color.White;
+			dataGridViewCellStyle48.SelectionBackColor = System.Drawing.Color.SteelBlue;
+			dataGridViewCellStyle48.SelectionForeColor = System.Drawing.Color.White;
+			this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle48;
+			this.dataGridView1.RowTemplate.Height = 24;
+			this.dataGridView1.Size = new System.Drawing.Size(647, 284);
+			this.dataGridView1.TabIndex = 0;
+			this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick_1);
+			// 
+			// buttonUpdate
+			// 
+			this.buttonUpdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(58)))), ((int)(((byte)(81)))));
+			this.buttonUpdate.Font = new System.Drawing.Font("Century Gothic", 12F);
+			this.buttonUpdate.ForeColor = System.Drawing.Color.WhiteSmoke;
+			this.buttonUpdate.Location = new System.Drawing.Point(365, 284);
+			this.buttonUpdate.Name = "buttonUpdate";
+			this.buttonUpdate.Size = new System.Drawing.Size(105, 55);
+			this.buttonUpdate.TabIndex = 6;
+			this.buttonUpdate.Text = "Update";
+			this.buttonUpdate.UseVisualStyleBackColor = false;
+			this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+			// 
+			// buttonDelete
+			// 
+			this.buttonDelete.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(58)))), ((int)(((byte)(81)))));
+			this.buttonDelete.Font = new System.Drawing.Font("Century Gothic", 12F);
+			this.buttonDelete.ForeColor = System.Drawing.Color.WhiteSmoke;
+			this.buttonDelete.Location = new System.Drawing.Point(238, 284);
+			this.buttonDelete.Name = "buttonDelete";
+			this.buttonDelete.Size = new System.Drawing.Size(108, 55);
+			this.buttonDelete.TabIndex = 7;
+			this.buttonDelete.Text = "Delete";
+			this.buttonDelete.UseVisualStyleBackColor = false;
+			this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Heebo", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(50, 54);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(142, 19);
+			this.label1.TabIndex = 8;
+			this.label1.Text = "Название фильма";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Heebo", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(50, 105);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(50, 19);
+			this.label2.TabIndex = 9;
+			this.label2.Text = "Жанр";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Heebo", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.ForeColor = System.Drawing.Color.White;
+			this.label3.Location = new System.Drawing.Point(52, 153);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(101, 19);
+			this.label3.TabIndex = 10;
+			this.label3.Text = "Год выпуска";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Heebo", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.ForeColor = System.Drawing.Color.White;
+			this.label4.Location = new System.Drawing.Point(52, 206);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(69, 19);
+			this.label4.TabIndex = 11;
+			this.label4.Text = "Рейтинг";
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(48)))), ((int)(((byte)(69)))));
+			this.panel1.Controls.Add(this.button);
+			this.panel1.Controls.Add(this.label4);
+			this.panel1.Controls.Add(this.label1);
+			this.panel1.Controls.Add(this.textBox3);
+			this.panel1.Controls.Add(this.textBox2);
+			this.panel1.Controls.Add(this.label3);
+			this.panel1.Controls.Add(this.buttonDelete);
+			this.panel1.Controls.Add(this.buttonUpdate);
+			this.panel1.Controls.Add(this.label2);
+			this.panel1.Controls.Add(this.textBox1);
+			this.panel1.Controls.Add(this.textBox4);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+			this.panel1.Location = new System.Drawing.Point(644, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(512, 386);
+			this.panel1.TabIndex = 12;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Copperplate Gothic Light", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.ForeColor = System.Drawing.Color.White;
+			this.label5.Location = new System.Drawing.Point(62, 23);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(190, 25);
+			this.label5.TabIndex = 13;
+			this.label5.Text = "List of Movies";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(58)))), ((int)(((byte)(81)))));
+			this.ClientSize = new System.Drawing.Size(1156, 386);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "List of Movies";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.Button button;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.ComponentModel.BackgroundWorker backgroundWorker1;
+		private System.Windows.Forms.Button buttonUpdate;
+		private System.Windows.Forms.Button buttonDelete;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Label label5;
+	}
+}
+
---
+++ MovieDataBase/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+
+
+//CRUD application
+namespace MovieDataBase
+{
+	public partial class Form1 : Form
+	{
+		SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["MovieDB"].ConnectionString);
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		int ID = 0;
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			LoadData();
+		}
+
+		private void LoadData()
+		{
+			try
+			{
+				sqlConnection.Open();
+				SqlDataAdapter dataAdapter = new SqlDataAdapter(
+					"SELECT * From [Movies]", sqlConnection);
+
+				SqlCommandBuilder sqlCommandBuilder = new SqlCommandBuilder(dataAdapter);
+
+				DataSet dataSet = new DataSet();
+				dataAdapter.Fill(dataSet, "Movies");
+				dataGridView1.DataSource = dataSet.Tables["Movies"];
+				dataGridView1.ReadOnly = true;
+				dataGridView1.AllowUserToAddRows = false;
+				sqlConnection.Close();
+
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+
+		private void ClearData()
+		{
+			textBox1.Text = "";
+			textBox2.Text = "";
+			textBox3.Text = null;
+			textBox4.Text = null;
+		}
+
+
+		private void button1_Click(object sender, EventArgs e)// Insert
+		{
+			if (textBox1.Text != "")
+			{
+				SqlCommand command = new SqlCommand(
+				$"INSERT INTO [Movies] (Title, Genre, YearDate, Rating) VALUES (@Title, @Genre, @YearDate, @Rating)",
+				sqlConnection);
+
+				sqlConnection.Open();
+
+				command.Parameters.AddWithValue("Title", textBox1.Text);
+				command.Parameters.AddWithValue("Genre", textBox2.Text);
+				if (int.TryParse(textBox3.Text, out int yeardate))
+					command.Parameters.AddWithValue("YearDate", yeardate);
+				else
+					command.Parameters.AddWithValue("YearDate", DBNull.Value);
+
+
+				if (int.TryParse(textBox4.Text, out int rating))
+					command.Parameters.AddWithValue("Rating", rating);
+				else
+					command.Parameters.AddWithValue("Rating", DBNull.Value);
+
+
+
+
+				command.ExecuteNonQuery();
+				sqlConnection.Close();
+				LoadData();
+				ClearData();
+			}
+			else
+			{
+				MessageBox.Show("Введите название");
+			}
+
+		}
+
+		private void buttonUpdate_Click(object sender, EventArgs e)
+		{
+			if (textBox1.Text != "")
+			{
+				SqlCommand command = new SqlCommand(
+					$"UPDATE Movies SET Title = @Title, Genre = @Genre, YearDate = @YearDate, Rating = @Rating where ID = @id",
+				sqlConnection);
+
+				sqlConnection.Open();
+
+				command.Parameters.AddWithValue("Title", textBox1.Text);
+				command.Parameters.AddWithValue("Genre", textBox2.Text);
+
+				if (int.TryParse(textBox3.Text, out int yeardate))
+					command.Parameters.AddWithValue("YearDate", yeardate);
+				else
+					command.Parameters.AddWithValue("YearDate", DBNull.Value);
+
+
+				if (int.TryParse(textBox4.Text, out int rating))
+					command.Parameters.AddWithValue("Rating", rating);
+				else
+					command.Parameters.AddWithValue("Rating", DBNull.Value);
+
+				command.Parameters.AddWithValue("id", ID);
+
+				command.ExecuteNonQuery();
+
+				sqlConnection.Close();
+				LoadData();
+				ClearData();
+			}
+			else
+			{
+				MessageBox.Show("Выберите запись для обновления");
+			}
+		}
+
+		private void buttonDelete_Click(object sender, EventArgs e)
+		{
+			if (ID != 0)
+			{
+				SqlCommand command = new SqlCommand(
+				$"DELETE [Movies] WHERE ID=@id",
+				sqlConnection);
+
+				sqlConnection.Open();
+
+				command.Parameters.AddWithValue("id", ID);
+				command.ExecuteNonQuery();
+				sqlConnection.Close();
+				LoadData();
+				ClearData();
+			}
+			else
+			{
+				MessageBox.Show("Выберите запись для удаления");
+			}
+		}
+
+
+		private void dataGridView1_RowHeaderMouseClick_1(object sender, DataGridViewCellMouseEventArgs e)
+		{
+			ID = Convert.ToInt32(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+			textBox1.Text = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+			textBox2.Text = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+			textBox3.Text = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+			textBox4.Text = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+		}
+	}
+}
